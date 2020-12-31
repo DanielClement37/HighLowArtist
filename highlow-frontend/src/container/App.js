6a1651a02eef676991f926880b6401dfe2427ad9
DanielClement37 <daniel.clement37@gmail.com>
2020-12-30 22:06:27 -0600

    Setup file structure for frontend

---
+++ highlow-frontend/src/container/App.js
@@ -0,0 +1,14 @@
+import NavBar from '../components/NavBar';
+import Game from '../components/Game'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
